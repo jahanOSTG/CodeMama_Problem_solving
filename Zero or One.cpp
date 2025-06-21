23a89a66948bb4f81a43ce4bd0e4bcd3950629b1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-21 23:50:02 +0600

    Create Zero or One.cpp

---
+++ Zero or One.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+ 
+    if (s == "zero") cout << 0 << endl;
+    else if (s == "one") cout << 1 << endl;
+    else if (s == "two") cout << 0 << endl;
+    else if (s == "three") cout << 1 << endl;
+    else if (s == "four") cout << 0 << endl;
+    else if (s == "five") cout << 1 << endl;
+    else if (s == "six") cout << 0 << endl;
+    else if (s == "seven") cout << 1 << endl;
+    else if (s == "eight") cout << 0 << endl;
+    else if (s == "nine") cout << 1 << endl;
+
+    return 0;
+}
