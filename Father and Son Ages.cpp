cbd034b00fdc59e3c47dd71a293fe2ca084d2db7
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-22 02:03:59 +0600

    Create Father and Son Ages.cpp

---
+++ Father and Son Ages.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+
+  
+  int age(int fAge,int sAge){
+    return fAge-(sAge*2);
+  }
+  int main() {
+
+  int fAge,sAge;
+  cin>>fAge>>sAge;
+  int result=age(fAge,sAge);
+  cout<<abs(result)<<endl;
+
+    return 0;
+}
