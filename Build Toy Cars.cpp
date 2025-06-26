e798325ca31b49387a4950ca63f2e7d9c36ab640
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-27 00:34:15 +0600

    Add files via upload

---
+++ Build Toy Cars.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int c,b,f;
+    cin>>c>>b>>f;
+    int x=c/4;
+    int y=f/2;
+    int z=min({x,b,y});
+    cout<<z<<endl;
+
+
+
+
+
+
+
+    }
+
