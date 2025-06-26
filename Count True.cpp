a4b0c94980a310d5697d0a284309cf7acac45a97
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-27 02:09:31 +0600

    Add files via upload

---
+++ Count True.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int arr[t];
+    int count=0;
+    for(int i=0;i<t;i++){
+        cin>>arr[i];
+
+        if(arr[i]==1){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+
+
+
+
+}
+
