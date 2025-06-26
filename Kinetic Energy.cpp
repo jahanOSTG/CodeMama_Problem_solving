388ed4081ba0a0a4b7a6205d838c43f513dbfbc8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-27 02:21:23 +0600

    Create Kinetic Energy.cpp

---
+++ Kinetic Energy.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double m, v;
+    cin >> m >> v;
+
+    double x = 0.5 * m * v * v;
+
+    cout << fixed << setprecision(0) << x << endl;
+
+    return 0;
+}
