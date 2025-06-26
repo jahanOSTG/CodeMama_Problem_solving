2d2b11dbf43b6bf4c958daa87d4309aecc5f5f4b
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-26 23:04:36 +0600

    Add files via upload

---
+++ Classify Evens.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[6];
+    int t;
+     int p=0;
+    for(int i = 0; i < 6; i++) {
+        cin >> arr[i];
+
+        if(arr[i]%2==0){
+            p=p+1;
+        }
+
+    }
+    cout<<p<<endl;
+
+
+
+
+
+    }
+
