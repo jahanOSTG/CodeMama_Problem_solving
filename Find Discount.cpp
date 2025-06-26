43c3f5eb6bad4a8e4cbcdb613e257025de335dca
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-26 23:21:26 +0600

    Add files via upload

---
+++ Find Discount.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float p,d;
+    cin>>p>>d;
+    float result=(p/100)*(100-d);
+     cout << fixed << setprecision(2);
+
+    cout<<"Price:"<<" "<< result<<endl;
+
+
+
+
+
+
+    }
+
