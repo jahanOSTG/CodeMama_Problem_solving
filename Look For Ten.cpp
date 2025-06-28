42c040f93842e8a82569b1ffd85f1fdbd528513d
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-28 19:01:23 +0600

    Create Look For Ten.cpp

---
+++ Look For Ten.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a,b;
+    cin>>a>>b;
+    if(a==10 || b==10 || (a+b)==10){
+        cout<<"True"<<endl;
+    }else{
+        cout<<"False"<<endl;
+    }
+
+    return 0;
+}
