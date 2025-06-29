03b6acf9637c77c3e1a217c88656e377ab505e4d
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-29 23:59:43 +0600

    Create Find Factorial.cpp

---
+++ Find Factorial.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a,n;
+    cin>>a;
+     n=1;
+    for(int i=1;i<=a;i++){
+
+        n=n*i;
+
+    }
+    cout<<n<<endl;
+
+    return 0;
+}
