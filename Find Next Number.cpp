513fae252b818942e3c963b44444fabfe69c9ebe
Faria Jahan Janie <janiefaria315@gmail.com>
2025-06-30 22:33:04 +0600

    Create Find Next Number.cpp

---
+++ Find Next Number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a,b,c;
+    cin>>a>>b>>c;
+    int e=(b-a);
+    cout<<c+e<<endl;
+
+    return 0;
+}
