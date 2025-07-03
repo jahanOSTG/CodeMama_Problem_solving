2a0a853e13562fdcc33506bd0dd437ee47c0fb56
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-04 01:49:59 +0600

    Add files via upload

---
+++ Present or Absent.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    bool found=false;
+
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            found=true;
+            break;
+
+
+    }
+    }
+    if(found){
+              cout << "P" << endl;
+        } else {
+            cout << "A" << endl;
+        }
+
+
+    return 0;
+}
