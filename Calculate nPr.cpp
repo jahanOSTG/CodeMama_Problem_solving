2a5eba9103f7acacbf6f17e46750613ac8b281ec
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-03 19:35:33 +0600

    Add files via upload

---
+++ Calculate nPr.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int factorial( int n){
+    int fact=1;
+    for(int i=1; i<=n ;i++){
+        fact=fact*i;
+
+    }
+      return fact;
+}
+
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int n,r;
+    cin>>n>>r;
+    int ans=factorial(n)/factorial(n-r);
+    cout<<ans<<endl;
+
+    return 0;
+}
