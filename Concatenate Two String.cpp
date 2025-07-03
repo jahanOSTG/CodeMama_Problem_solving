2c6951f5906c4e1a515ff5f279d30a61649cd2f8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-04 01:56:43 +0600

    Create Concatenate Two String.cpp

---
+++ Concatenate Two String.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    string first,last;
+    cin>>first>>last;
+    cout<<last<<", "<<first<<endl;
+    return 0;
+
+    return 0;
+}
