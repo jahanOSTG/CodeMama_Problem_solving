ef606de78c7d4c9dd8d98a2a2a99574ea21360d1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-05 20:18:56 +0600

    Create Three Number Average.cpp

---
+++ Three Number Average.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+     
+int main() {
+    float a,b,c;
+    cin>>a>>b>>c;
+    float result=(a+b+c)/3;
+    cout<<"Average: "<<fixed << setprecision(2) <<result<<endl;
+
+    return 0;
+}
