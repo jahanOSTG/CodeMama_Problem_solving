41da83a8700a2021ba3b988473c2f2793cf72f56
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-05 20:08:23 +0600

    Create Find Difference.cpp

---
+++ Find Difference.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a,b;
+    cin>>a>>b;
+    cout<<a-b<<endl;
+
+    return 0;
+}
