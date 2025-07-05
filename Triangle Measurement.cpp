e9740fa23fc8bfe88b6517435c91b3ec76979ff4
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-05 20:24:00 +0600

    Create Triangle Measurement.cpp

---
+++ Triangle Measurement.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a,b;
+    cin>>a>>b;
+    float result=(0.5)*a*b;
+    cout<<int(result)<<endl;
+
+    return 0;
+}
