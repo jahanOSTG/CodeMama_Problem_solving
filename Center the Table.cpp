34b77413ae20ea5b196ca9a093643d1b15d970ec
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-06 10:09:16 +0600

    Create Center the Table.cpp

---
+++ Center the Table.cpp
@@ -0,0 +1,15 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    /**
+    * Write your C++ code from here
+    */
+    int a;
+    cin>>a;
+    int result=(a-300)/2;
+    cout<<result<<endl;
+
+    return 0;
+}
