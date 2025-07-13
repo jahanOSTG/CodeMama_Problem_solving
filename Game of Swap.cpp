82f2fbe4f2cfb2f1aa496e4967696675e394bb28
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-13 22:56:28 +0600

    Create Game of Swap.cpp

---
+++ Game of Swap.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+     
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<"Before swapping: num1 = "<<a<<", num2 = "<<b<<endl;
+    swap(a,b);
+    cout<<"After swapping: num1 = "<<a<<", num2 = "<<b<<endl;
+    return 0;
+}
