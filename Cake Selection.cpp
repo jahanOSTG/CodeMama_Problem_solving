bd07b684659f5cd2b9fe8be15a18487cd820ae74
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-14 18:39:52 +0600

    Create Cake Selection.cpp

---
+++ Cake Selection.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a >= 200 && a <= 300 && b >= 50 && c >= 150) {
+         cout << "Yes" << endl;
+            }
+    else {
+            cout << "No" << endl;
+        }
+   
+
+    return 0;
+}
