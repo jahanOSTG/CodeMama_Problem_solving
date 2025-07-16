5effa5e8ff46206eedce67104546f424d64713f8
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-16 22:48:30 +0600

    Create Gamble with Profit.cpp

---
+++ Gamble with Profit.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+     
+int main() {
+    float a;
+    int b,c;
+    cin>>a>>b>>c;
+    float result=a*b;
+    if(result>c) cout<<"true"<<endl;
+    else cout<<"false"<<endl;
+    return 0;
+}
