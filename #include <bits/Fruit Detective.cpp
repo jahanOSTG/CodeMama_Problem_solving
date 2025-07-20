689dc1fb05ae2aeef808d6272ee1ec54ce4fe3b6
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-21 00:39:08 +0600

    Create Fruit Detective.cpp

---
+++ #include <bits/Fruit Detective.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int id;
+    cin>>id;
+    if(id==31231){
+        cout<<"Banana"<<endl;
+    }else if(id==43861){
+        cout<<"Elderberry"<<endl;
+    }else if(id==82678){
+        cout<<"Honeydew Melon"<<endl;
+    }else if(id==23456){
+        cout<<"Apple"<<endl;
+    }else if(id==78901){
+        cout<<"Mango"<<endl;
+    }else if(id==98765){
+        cout<<"Nectarine"<<endl;
+    }else if(id==45678){
+        cout<<"Orange"<<endl;
+    }else if(id==67890){
+        cout<<"Raspberry"<<endl;
+    }else{
+        cout<<"Tangerine"<<endl;
+    }
+
+    return 0;
+}
