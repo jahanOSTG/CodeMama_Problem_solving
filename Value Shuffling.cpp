2d6a6511976b1ba9228c988bc6b25ca48c16e5c1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-08-08 23:09:20 +0600

    Create Value Shuffling.cpp

---
+++ Value Shuffling.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+ #include<string>
+ #include<algorithm>
+ using namespace std;
+ int main(){
+     int a,b,c;
+     string str;
+     cin>>a>>b>>c;
+     cin>>str;
+
+
+
+
+     for(int i=0;i<str.length();i++){
+        char ch=str[i];
+        if (ch == 'A') cout << c << " ";
+        else if (ch == 'B') cout << a << " ";
+        else if (ch == 'C') cout << b << " ";
+     }
+
+ }
